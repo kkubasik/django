a0b7fd6181e9a86218b1de8cbdbc4e80fe1d1c35
Kevin Kubasik <kkubasik@kevin-kubasiks-macbook.local>
2009-05-28 02:09:19 -0600

    Doing a little  abstraction here and there

--- django/test/test_coverage.py
+++ django/test/test_coverage.py
@@ -14,48 +14,87 @@ def _get_app_package(app_model_module):
     """
     return '.'.join(app_model_module.__name__.split('.')[:-1])
 
-def run_tests(test_labels, verbosity=1, interactive=True,
-              extra_tests=[]):
-    """
-    Test runner which displays a code coverage report at the end of the
-    run.
-    """
-    #coverage.use_cache(0)
-    for e in getattr(settings, 'COVERAGE_CODE_EXCLUDES', []):
-        coverage.exclude(e)
-    coverage.start()
-    results = base_run_tests(test_labels, verbosity, interactive, extra_tests)
-    coverage.stop()
 
-    coverage_modules = []
-    if test_labels:
-        for label in test_labels:
-            label = label.split('.')[0]
-            app = get_app(label)
-            coverage_modules.append(_get_app_package(app))
-    else:
-        for app in get_apps():
-            coverage_modules.append(_get_app_package(app))
+class BaseCoverageRunner(object):
+    """"""
 
-    coverage_modules.extend(getattr(settings, 'COVERAGE_ADDITIONAL_MODULES', []))
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        pass
 
-    packages, modules, excludes, errors = get_all_modules(
-        coverage_modules, getattr(settings, 'COVERAGE_MODULE_EXCLUDES', []),
-        getattr(settings, 'COVERAGE_PATH_EXCLUDES', []))
+    #----------------------------------------------------------------------
+    def run_tests(test_labels, verbosity=1, interactive=True,
+                  extra_tests=[]):
+        """
+        Test runner which displays a code coverage report at the end of the
+        run.
+        """
+        #coverage.use_cache(0)
+        for e in getattr(settings, 'COVERAGE_CODE_EXCLUDES', []):
+            coverage.exclude(e)
+        coverage.start()
+        results = base_run_tests(test_labels, verbosity, interactive, extra_tests)
+        coverage.stop()
 
-    coverage.report(modules.values(), show_missing=1)
-    if excludes:
+        coverage_modules = []
+        if test_labels:
+            for label in test_labels:
+                label = label.split('.')[0]
+                app = get_app(label)
+                coverage_modules.append(_get_app_package(app))
+        else:
+            for app in get_apps():
+                coverage_modules.append(_get_app_package(app))
+
+        coverage_modules.extend(getattr(settings, 'COVERAGE_ADDITIONAL_MODULES', []))
+
+        packages, modules, excludes, errors = get_all_modules(
+            coverage_modules, getattr(settings, 'COVERAGE_MODULE_EXCLUDES', []),
+            getattr(settings, 'COVERAGE_PATH_EXCLUDES', []))
+
+        coverage.report(modules.values(), show_missing=1)
+        if excludes:
+            print >>sys.stdout
+            print >>sys.stdout, "The following packages or modules were excluded:",
+            for e in excludes:
+                print >>sys.stdout, e,
+            print >>sys.stdout
+        if errors:
+            print >>sys.stdout
+            print >>sys.stderr, "There were problems with the following packages or modules:",
+            for e in errors:
+                print >>sys.stderr, e,
+            print >>sys.stdout
+        return results
+
+
+class ReportingCoverageRunner(BaseCoverageRunner):
+    """Reporting"""
+
+    def __init__(self, outdir = None):
+        """Constructor"""
+        if(outdir):
+            self.outdir = outdir
+        else:
+            # Realistically, we aren't going to ship the entire reporting framework..
+            # but for the time being I have left it in.
+            self.outdir = getattr(settings, 'COVERAGE_REPORT_HTML_OUTPUT_DIR', 'test_html')
+            self.outdir = os.path.abspath(self.outdir)
+            # Create directory
+            os.mkdir(self.outdir)
+
+
+    def run_tests(self, *args, **kwargs):
+        """"""
+        res = BaseCoverageRunner.run_tests(self, *args, **kwargs)
+        html_report(self.outdir, modules, excludes, errors)
         print >>sys.stdout
-        print >>sys.stdout, "The following packages or modules were excluded:",
-        for e in excludes:
-            print >>sys.stdout, e,
-        print >>sys.stdout
-    if errors:
-        print >>sys.stdout
-        print >>sys.stderr, "There were problems with the following packages or modules:",
-        for e in errors:
-            print >>sys.stderr, e,
-        print >>sys.stdout
+        print >>sys.stdout, "HTML reports were output to '%s'" %outdir
 
-    return results
+        return res    
+
+
+
+
 
